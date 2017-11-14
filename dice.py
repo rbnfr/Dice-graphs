6f3bb79c7836a87cf23d93c710c8b8169455b1ca
Ruben_fr <ruben.f.alcaraz@gmail.com>
2017-11-14 16:03:03 +0100

    Little refactoring

--- dice.py
+++ dice.py
@@ -15,19 +15,17 @@ def main():
     max_points = 0
 
     # USER VARIABLES
-    rolls = 10000
+    rolls = 1000
     dice_list = {
-        'D20': 20, 
-        'D4': 4,
-        'D6': 6,
-        'D5': 5
+        'D6'  : 6, 
+        'D6_2': 6      
         }    
 
     # HISTOGRAM RANGE
     min_points = len(dice_list)
 
     for faces in dice_list.values():
-        max_points = max_points + faces
+        max_points += faces
 
     possible_points = range(min_points,max_points+1)
 
@@ -35,7 +33,7 @@ def main():
     for i in range(rolls):
         points = 0
         for dice in dice_list.values():
-            points = points +  randint(1, dice)
+            points += randint(1, dice)
         points_list.append(points)    
 
     # BUILD GRAPH
