83b7437a9f48bdc0abe98efee095ab9891265ea5
Ruben_fr <ruben.f.alcaraz@gmail.com>
2018-01-04 13:37:36 +0100

    Handled exceptions. Moar jokes

--- dice.py
+++ dice.py
@@ -21,25 +21,29 @@ def main():
         'D6'  : 6, 
         'D6_2': 6
         }
- 
+
+    try:
     # HISTOGRAM RANGE
-    min_points = histController.minPoints(dice_list)
-    max_points = histController.maxPoints(dice_list) 
-    possible_points = range(min_points,max_points+1)
+        min_points = histController.minPoints(dice_list)
+        max_points = histController.maxPoints(dice_list)
+        possible_points = range(min_points,max_points+1)
  
     # GET DATA
-    points_list = diceController.doRolls(dice_list, rolls)
-    points_freq = diceController.storeFrequency(possible_points, points_list)
-    print(points_freq)
+        points_list = diceController.doRolls(dice_list, rolls)
+        points_freq = diceController.storeFrequency(possible_points, points_list)
     
-    # BUILD GRAPH
-    fin = [possible_points.index(i) for i in points_list]
-    plt.hist(fin, bins=range(max_points), align="left", color="blue", rwidth=0.8)
-    plt.xticks(range(max_points), possible_points)
- 
-    plt.xlabel("Results")
-    plt.ylabel("Frequency")
-    plt.show()
-    
+    # BUILD GRAPH    
+        fin = [possible_points.index(i) for i in points_list]
+        plt.hist(fin, bins=range(max_points), align="left", color="blue", rwidth=0.8)
+        plt.xticks(range(max_points), possible_points)
+        plt.xlabel("Results")
+        plt.ylabel("Frequency")
+        print("Frequencies:", points_freq)
+        plt.show()
+    except (KeyboardInterrupt, SystemExit):
+        raise
+    except:
+        print("I refuse to work in this conditions.")
+
 if __name__ == '__main__':
     main()
--- histModel.py
+++ histModel.py
@@ -16,8 +16,13 @@ class histogram(object):
         index = 0
         max_points = 0
         for faces in dice_list.values():
-            if faces < 4:
-                print("Invalid faces at dice " + str(index+1) + ", are you a fucking retard?")
+            if faces < 4 or faces != int(faces):
+                print("Invalid faces at dice " + str(index + 1) + ", are you a fucking retard?")
+                if (faces).is_integer() is False:
+                    print("Decimal faces? Are you fucking kidding me?")
+                break
+            elif faces >= 100:
+                print("\"Dice\"",str(index+1),"is not a dice, is a fucking sphere.")
 
             index+=1
             max_points += faces
